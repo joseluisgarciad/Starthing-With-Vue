5fd53e06273760c5d1f69d7b3d8b6ed86e0b3060
joseluis <joseluisgarciad@gmail.com>
2023-02-02 14:59:29 +0100

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <StartingWithVue msg="Welcome ro your starter component" class="border border-success"/><hr>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import StartingWithVue from './components/StartingWithVue.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    StartingWithVue,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/StartingWithVue.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="hello">
+        <div>
+            <h1>{{ msg }}</h1>
+            <p>
+             Hello World from Your Starter Component
+            </p>
+            <br/>
+            <p>
+                {{ count }} <br/>
+                <button @click="increment">-</button>
+                <button @click="decrement">+</button>
+            </p>
+        </div>
+        <br/>
+        <div>
+            <p>Mensaje original: "{{ msg }}"</p>
+            <p>Mensaje invertido computado: "{{ reverseMessage }}"</p>
+        </div>
+        <br/>
+        <div v-if="Math.random() > 0.5">
+            Ahora me ves
+        </div>
+        <div v-else>
+            Ahora no
+        </div>
+    </div>
+</template>
+
+<script>
+//import { ref } from "vue";
+
+export default {
+    name: 'StartingWithVue',
+    props: {
+        msg: {required: true, type: String},
+        type: {default: 'A', type: String}
+    },
+    data: () => ({
+        /*This data is private and only for the component itself to use.
+        other components do not have access to it*/
+        count: 0
+    }),
+    computed:{
+        reverseMessage:function() {
+            return this.msg.split('').reverse().join('')
+        }
+    }
+}
+</script>
